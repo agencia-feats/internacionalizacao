a2621f9452049ed78203d5abcb8d7d20a2a1b235
Israel Nogueira <israel@feats.com.br>
2019-12-10 01:38:23 -0300

    Update getLang.php

--- getLang.php
+++ getLang.php
@@ -1,21 +1,39 @@
 <?
 	function getlang($path = null, $isso = "", $porisso = "") {
+		####################################################################
+		# CAPTAMOS A STRING DO JSON E TRANSFORMAMOS EM OBJETO
+		####################################################################
 		$json  = str_replace(array(PHP_EOL, "\n", "\r"), "", file_get_contents('./pt-BR.json'));
 		$obj   = json_decode($json, TRUE);
+
+		####################################################################
+		# TRATAMOS A RODA DA STRING
+		####################################################################
 		$paths = explode(">",str_replace(" ","",$path));
+
+
 		if ($path == null) {
-			$content = "Invalid path ws::getlang( ? )";
+			$content = "Invalid path getlang( ? )";
 		} elseif ($obj == '') {
 			$content = "JSON error";
 		} else {
+			####################################################################
+			# VARREMOS A ARRAY DA ROTA
+			####################################################################
 			foreach ($paths as $value) {
 				if (empty($obj[trim($value)]) && empty($content[trim($value)])) {
 					$content = "Path " . $path . " does not exist";
 					break;
 				} else {
+					####################################################################
+					# GRAVA A VARIÁVEL COM A STRING REQUISITADA
+					####################################################################
 					$content = (empty($content)) ? $obj[trim($value)] : $content[trim($value)];
 				}
 			}
 		}
+		####################################################################
+		# RETORNAMOS A STRING, SUBSTITUINDO O QUE OS PARÂMETROS PEDEM
+		####################################################################
 		return str_replace($isso, $porisso, $content);
 	}
